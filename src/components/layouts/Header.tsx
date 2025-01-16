d083a3580fb214b2f6b5bbde092f4d39e2c1bccb
jaspreet tpss <jaspreetstpss@gmail.com>
2025-01-16 17:29:16 +0530

    mobile menu

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -8,7 +8,15 @@ function Header() {
     <header className="py-2 sm:py-4 bg-white lg:fixed lg:top-10 w-full z-10">
       <div className="dz_container_large">
         <div className="flex items-center justify-between gap-2">
-          <div className="max-md:max-w-[130px] text-2xl xl:text-3xl logo">GREENKIVE</div>
+          <div className=" text-2xl xl:text-3xl logo flex-1 md:text-left text-center">GREENKIVE</div>
+          <div className=" md:hidden flex-1 -order-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 20 20" ><path d="M19 5H1c-.6 0-1-.4-1-1s.4-1 1-1h18c.6 0 1 .4 1 1s-.4 1-1 1m-9 6H1c-.6 0-1-.4-1-1s.4-1 1-1h9c.6 0 1 .4 1 1s-.4 1-1 1m9 6H1c-.6 0-1-.4-1-1s.4-1 1-1h18c.6 0 1 .4 1 1s-.4 1-1 1" /></svg>
+            <ul className="hidden sm:flex flex-col list gap-4 bg-primary p-6 absolute left-0 top-[88px]  z-10 min-w-60 h-screen">
+              {navLinks.map((link) => (<Link href={link.href} key={link.key} className="text-sm md:text-base lg:text-md  text-white font-medium " >
+                {link.label}
+              </Link>))}
+            </ul>
+          </div>
           <div className="flex space-x-10 items-center">
             <ul className=" hidden lg:flex list gap-7 xl:gap-16">
               {navLinks.map((link) => (<Link href={link.href} key={link.key} className="text-sm md:text-base lg:text-md  text-primary font-medium " >
@@ -16,8 +24,8 @@ function Header() {
               </Link>))}
             </ul>
           </div>
-          <div className="flex items-center space-x-5 xl:space-x-10">
-            <div className="relative hidden sm:block ">
+          <div className="flex items-center space-x-3 xl:space-x-10  flex-1 justify-end">
+            <div className="relative hidden md:block ">
               <input
                 type="text"
                 placeholder="Search"
@@ -28,7 +36,7 @@ function Header() {
               </div>
 
             </div>
-            <div className="block sm:hidden">
+            <div className="block md:hidden">
               {searchIcon}
             </div>
             <div className="relative">{userIcon}</div>
